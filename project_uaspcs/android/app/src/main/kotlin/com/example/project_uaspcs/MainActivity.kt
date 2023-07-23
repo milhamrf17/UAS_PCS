7f1d44de4824d450ff58d8c52853d638868725f0
m.ilhamrf17 <115083316+milhamrf17@users.noreply.github.com>
2023-07-23 21:18:58 +0700

    Add files via upload

---
+++ project_uaspcs/android/app/src/main/kotlin/com/example/project_uaspcs/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.project_uaspcs
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ project_uaspcs/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<shared_preferences/SharedPreferencesPlugin.h>)
+#import <shared_preferences/SharedPreferencesPlugin.h>
+#else
+@import shared_preferences;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FLTSharedPreferencesPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTSharedPreferencesPlugin"]];
+}
+
+@end
